ae319da2c856ce8e668ef7d38f36052a10113bb6
DMITRI <nic.dima11@mail.ru>
2025-05-20 23:09:03 +0300

    create helper methods for sending API requests related to students

--- src/test/java/org/example/api/StudentRequests.java
+++ src/test/java/org/example/api/StudentRequests.java
@@ -1,24 +1,35 @@
 package org.example.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.http.ContentType;
+import io.restassured.mapper.ObjectMapperType;
 import org.apache.http.HttpStatus;
+import org.example.api.models.Student;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.hasKey;
 
 public class StudentRequests {
-    public static String createStudent(String body) {
+
+    public static Student createStudent(Student student) {
+
+        String studentJson = JsonUtil.toJson(student);
+
         return given()
                 .header("Content-Type", "application/json")
-                .body(body)
+                .body(studentJson)
                 .when()
                 .post("/student")
                 .then()
                 .assertThat()
                 .statusCode(201)
                 .body("$", hasKey("_id"))
-                .extract().path("_id");
+                .extract().as(Student.class, ObjectMapperType.GSON);
     }
-    public static void deleteStudent(String id){
+
+    public static void deleteStudent(String id) {
         given()
                 .delete("/student/" + id)
                 .then()
