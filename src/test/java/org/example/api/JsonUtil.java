279505f1a835ae9b4202ddd700264f686e3f1d50
DMITRI <nic.dima11@mail.ru>
2025-05-20 23:07:29 +0300

    add utility class for parsing JSON responses in API tests

---
+++ src/test/java/org/example/api/JsonUtil.java
@@ -0,0 +1,17 @@
+package org.example.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Ошибка при сериализации объекта в JSON", e);
+        }
+    }
+}
